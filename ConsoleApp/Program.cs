c0d98c57acb4c3a1d367d6fd3b4a9aa9597d32f9
Elafifi Nour-Eldin <59283990+Nour-ElMasry@users.noreply.github.com>
2022-07-28 19:09:02 +0300

    first commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+using Vehicles;
+
+
+var carsList = Car.GenerateCars();
+
+var FilterByProductionDate = carsList.Where(c => c.ProductionDate.Year <= 2019)
+    .OrderByDescending(c => c.ProductionDate)
+    .Select(c => new { c.Manufacturer, c.ProductionDate.Year });
+
+//printThis(FilterByProductionDate);
+
+var tmpListOfYears = new List<DateTime>
+{
+    new DateTime(2019,1,1),
+    new DateTime(2011,2,2)
+};
+
+var joinBothLists = from p in carsList
+                    join ls in tmpListOfYears
+                    on p.ProductionDate.Year equals ls.Year
+                    select new
+                    {
+                        p.Manufacturer,
+                        p.ProductionDate.Year
+                    };
+
+//printThis(joinBothLists);
+
+var tmpStrings = new List<string> 
+{ 
+    "Car 1",
+    "Car 2",
+    "Car 3"
+};
+
+var zipTest = FilterByProductionDate.Zip(tmpStrings, (c, y) => c.ToString() + " - " + y);
+
+//printThis(zipTest);
+
+var orderingTest = carsList.OrderByDescending(c => c.Price)
+    .ThenBy(c => c.ProductionDate.Year)
+    .Reverse();
+
+//printThis(orderingTest);
+
+var groupByTest = carsList.GroupBy(c => c.Manufacturer);
+
+//foreach(var group in groupByTest)
+//{
+//    Console.WriteLine(group.Key + " : " + group.Count() + "\n");
+
+//    printThis(group);
+//    Console.WriteLine("\n");
+//}
+
+var convertionMethod = carsList.ToDictionary(c => c.Id, c => c.Model);
+
+//printThis(convertionMethod);
+
+var elementOperators = orderingTest.First(c => c.Manufacturer == "Volkswagen");
+
+//printThis(orderingTest);
+//Console.WriteLine("\n/////////////////////////\n\nFirst Volkswagen in list is:");
+//Console.WriteLine(elementOperators);
+
+var AggregationMethods = orderingTest.Sum(c => c.Price);
+
+var AggregationMethods2 = orderingTest.Average(c => c.Price);
+
+//Console.WriteLine($"Sum of all car prices: {AggregationMethods}");
+//Console.WriteLine($"Average price of cars in list: {AggregationMethods2}");
+
+var quantifiers = carsList.All(c => c.ProductionDate.Year > 2010);
+
+var quantifiers2 = FilterByProductionDate.Contains(joinBothLists.Last());
+
+var quantifiers3 = carsList.Any(c => c.ProductionDate.Year < 2015);
+
+//Console.WriteLine($"All cars manufacutured after 2010? -> {quantifiers}");
+//Console.WriteLine($"Does last element from joinBothLists list exist in FilterByProductionDate List? -> {quantifiers2}");
+//Console.WriteLine($"Any cars manufactured before 2015? -> {quantifiers3}");
+
+static void printThis<T>(IEnumerable<T> items) 
+{
+    foreach (T item in items)
+    {
+        Console.WriteLine(item);
+    }
+}
---
+++ Vehicle/Car.cs
@@ -0,0 +1,43 @@
+﻿namespace Vehicles
+{
+    public class Car
+    {
+        static int idCounter = 0;
+        public int Id { get; set; }
+        public string Manufacturer { get; set; }
+
+        public string Model { get; set; }
+
+        public float Price { get; set; }
+
+        public DateTime ProductionDate { get; set; }
+
+        public Car(string manufacturer, string model, float price, DateTime productionDate)
+        {
+            Id = idCounter++;
+            Manufacturer = manufacturer;
+            Model = model;
+            Price = price;
+            ProductionDate = productionDate;
+        }
+
+        public static List<Car> GenerateCars() 
+        {
+            return new List<Car>
+            {
+                new Car("Porche", "Panamera", 82000, new DateTime(2018,1,1)),
+                new Car("BMW", "M4 coupe", 90000, new DateTime(2022,1,1)),
+                new Car("BMW", "M5", 125000, new DateTime(2022,1,1)),
+                new Car("Mercedes", "C class coupe", 35000, new DateTime(2019,1,1)),
+                new Car("Audi", "A1", 12000, new DateTime(2016,1,1)),
+                new Car("Volkswagen", "Golf", 28000, new DateTime(2021,1,1)),
+                new Car("Volkswagen", "ID.4", 55000, new DateTime(2022,1,1)),
+            }; 
+        }
+
+        public override string? ToString()
+        {
+            return $"Car => Manufacutrer: {Manufacturer}, Model: {Model}, Price: {Price}, Profuction date: {ProductionDate.Year}";
+        }
+    }
+}
